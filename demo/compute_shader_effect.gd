80573c94876f1ef3c04ab7c63315aacb03e6af2d
Devin <devin@nowyoujump.com>
2025-07-03 20:08:34 -0700

    rename ScreenTexture -> ColorTexture to match godot naming

--- demo/compute_shader_effect.gd
+++ demo/compute_shader_effect.gd
@@ -102,7 +102,7 @@ func _render_callback(p_effect_callback_type: int, p_render_data: RenderData) ->
 				)
 				for view in range(view_count):
 					for param: String in kernel.parameters:
-						if kernel.parameters[param].user_attributes.has("gd_compositor_ScreenTexture"):
+						if kernel.parameters[param].user_attributes.has("gd_compositor_ColorTexture"):
 							_task.set_shader_parameter(param, render_scene_buffers.get_color_layer(view))
 						if kernel.parameters[param].user_attributes.has("gd_compositor_DepthTexture"):
 							_task.set_shader_parameter(param, render_scene_buffers.get_depth_layer(view))
