30a23abb0ec94de5897ee4d8734a5481503e4460
Devin <devin@nowyoujump.com>
2025-10-04 00:23:57 -0700

    update the demo scene

--- demo/scripts/example.gd
+++ demo/scripts/example.gd
@@ -1,16 +1,8 @@
 extends Node3D
 
-@export var world_env: WorldEnvironment
-
 func _unhandled_input(event: InputEvent) -> void:
-	var compositor := world_env.compositor
 	if event.is_action_pressed("toggle_data_mosh"):
-		var index := compositor.compositor_effects.find_custom(
-			func (effect: CompositorEffect):
-				return effect is ComputeShaderEffect and effect.compute_shader == preload("uid://drcr6fv2t323a")
-		)
-		if index >= 0:
-			var data_mosh_effect: ComputeShaderEffect = compositor.compositor_effects[index]
-			if data_mosh_effect:
-				data_mosh_effect.enabled = not data_mosh_effect.enabled
-				data_mosh_effect.queue_dispatch("capture_screen")
+		var data_mosh_effect: ComputeShaderEffect = load("res://example.tscn::ComputeShaderEffect_mt10o")
+		data_mosh_effect.enabled = not data_mosh_effect.enabled
+		data_mosh_effect.queue_dispatch("capture_screen")
+		get_viewport().set_input_as_handled()
