13a959a1a004c727af7564fd66d3fcb1be9b47f2
Devin <devin@nowyoujump.com>
2025-10-31 02:02:42 -0700

    move data_most_effect into @export property in demo project

--- demo/scripts/example.gd
+++ demo/scripts/example.gd
@@ -1,8 +1,9 @@
 extends Node3D
 
+@export var data_mosh_effect: ComputeShaderEffect
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("toggle_data_mosh"):
-		var data_mosh_effect: ComputeShaderEffect = load("res://example.tscn::ComputeShaderEffect_mt10o")
 		data_mosh_effect.enabled = not data_mosh_effect.enabled
 		data_mosh_effect.queue_dispatch("capture_screen")
 		get_viewport().set_input_as_handled()
